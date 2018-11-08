9a74ad0f95b9acc08edf377bb70345ea9912d211
bmaczka <bmaczka@umich.edu>
2018-11-08 13:44:46 -0500

    added most of the files

---
+++ app/src/main/java/com/example/blake/minibuddhav0/activity_old_notes.java
@@ -0,0 +1,13 @@
+package com.example.blake.minibuddhav0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_old_notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_notes);
+    }
+}
