b9e83bf0e8611af8a639ff082806b1bc1bab1f10
bmaczka <bmaczka@umich.edu>
2018-12-09 23:50:25 -0500

    Got basic timer functionality running on guided meditation activity

--- app/src/main/java/com/example/blake/minibuddhav0/GuidedMeditation.java
+++ app/src/main/java/com/example/blake/minibuddhav0/GuidedMeditation.java
@@ -1,13 +1,101 @@
 package com.example.blake.minibuddhav0;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GuidedMeditation extends AppCompatActivity {
+public class GuidedMeditation extends AppCompatActivity implements View.OnClickListener {
+
+    private Button fiveMinuteButton, tenMinuteButton, fifteenMinuteButton, startButton;
+    private TextView timerTextView;
+    private CountDownTimer countDownTimer;
+    private long startTimeInMillis = 600000;
+    private long timeLeftInMilliseconds = startTimeInMillis; //10 minutes
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guided_meditation);
+
+        fiveMinuteButton = findViewById(R.id.fiveMinuteButton);
+        tenMinuteButton = findViewById(R.id.tenMinuteButton);
+        fifteenMinuteButton = findViewById(R.id.fifteenMinuteButton);
+        timerTextView = findViewById(R.id.timerTextView);
+        startButton = findViewById(R.id.startButton);
+        fiveMinuteButton.setOnClickListener(this);
+        tenMinuteButton.setOnClickListener(this);
+        fifteenMinuteButton.setOnClickListener(this);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(timerRunning){
+                    stopTimer();
+                }
+                else{
+                    startTimer();
+                }
+            }
+        });
+
+        updateTimer();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == fiveMinuteButton){
+            //set timer to 05:00
+        }
+        else if(view == tenMinuteButton){
+            //set timer to 10:00
+        }
+        else if(view == fifteenMinuteButton){
+            //set timer to 15:00
+        }
+        else if(view == startButton){
+            //start the timer and update the text view to reflect the countdown
+            startTimer();
+        }
+    }
+
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMilliseconds = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+                startButton.setText("START");
+
+            }
+        }.start();
+        startButton.setText("PAUSE");
+        timerRunning = true;
+    }
+
+    public void stopTimer(){
+        countDownTimer.cancel();
+        startButton.setText("START");
+        timerRunning = false;
+    }
+
+    public void updateTimer(){
+    int minutes = (int) timeLeftInMilliseconds / 60000;
+    int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
+
+    String timeLeftText;
+    timeLeftText = "" + minutes;
+    timeLeftText += ":";
+    if(seconds < 10) timeLeftText += "0";
+    timeLeftText += seconds;
+
+    timerTextView.setText(timeLeftText);
     }
 }
--- app/src/main/java/com/example/blake/minibuddhav0/activity_old_notes.java
+++ app/src/main/java/com/example/blake/minibuddhav0/activity_old_notes.java
@@ -1,5 +1,6 @@
 package com.example.blake.minibuddhav0;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -7,6 +8,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -47,24 +49,20 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
     private String user = User.name;
     private String thingOne, thingTwo, thingThree, date, key;
     private NotificationUtils mNotificationUtils;
-    private boolean sorted = false;
-
-
     private ArrayList<Contact> contacts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_old_notes);
         initContacts();
         sortButton = findViewById(R.id.sortButton);
-        //sortAlphaButton = findViewById(R.id.sortAlphaButton);
         notificationsButton = findViewById(R.id.notificationsButton);
         toHomeButton = findViewById(R.id.toHomeButton);
         sortButton.setOnClickListener(this);
         notificationsButton.setOnClickListener(this);
         toHomeButton.setOnClickListener(this);
         mNotificationUtils = new NotificationUtils(this);
-        //sortAlphaButton.setOnClickListener(this);
 
         notificationsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +72,7 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         List<DataSnapshot> L = new ArrayList<>();
-                        for(DataSnapshot d: dataSnapshot.getChildren()){
+                        for (DataSnapshot d : dataSnapshot.getChildren()) {
                             L.add(d);
                         }
                         Random rand = new Random();
@@ -87,27 +85,29 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                         //String dateYear = Integer.toString(goodThingNotification.getDate().getYear());
                         String date = dateMonth + "/" + dateDay;
 
-                        if(!TextUtils.isEmpty(thing) && !TextUtils.isEmpty(date)) {
+                        if (!TextUtils.isEmpty(thing) && !TextUtils.isEmpty(date)) {
                             Notification.Builder nb = mNotificationUtils.getAndroidChannelNotification(thing, "recorded on:  " + date);
                             mNotificationUtils.getManager().notify(101, nb.build());
                         }
                     }
+
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
                     }
                 });
             }
         });
+
     }
 
-    private void initContacts(){
+    private void initContacts() {
         contacts = new ArrayList<>();
         dbref = FirebaseDatabase.getInstance().getReference(user);
         dbref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 List<DataSnapshot> L = new ArrayList<>();
-                for(DataSnapshot d: dataSnapshot.getChildren()){
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
                     L.add(d);
                 }
                 Random rand = new Random();
@@ -123,7 +123,7 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                 GoodThings goodThingsChosen2 = new GoodThings();
                 goodThingsChosen2 = L.get(randIndex2).getValue(GoodThings.class);
                 //if below ensures that the same set of things isn't selected twice
-                if(goodThingsChosen2.getThingOne() == thingOne){
+                if (goodThingsChosen2.getThingOne() == thingOne) {
                     rand2 = new Random();
                 }
                 randIndex2 = rand2.nextInt(L.size());
@@ -137,7 +137,7 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                 GoodThings goodThingsChosen3 = new GoodThings();
                 goodThingsChosen3 = L.get(randIndex3).getValue(GoodThings.class);
                 //if below ensures that the same set of things isn't selected twice
-                if(goodThingsChosen3.getThingOne() == thingOne || goodThingsChosen3.getThingOne() == thingTwo){
+                if (goodThingsChosen3.getThingOne() == thingOne || goodThingsChosen3.getThingOne() == thingTwo) {
                     rand3 = new Random();
                 }
                 randIndex3 = rand3.nextInt(L.size());
@@ -147,6 +147,7 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                 contacts.add(new Contact(goodThingsChosen3.getThingThree()));
                 initRecyclerView();
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
@@ -154,7 +155,7 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
         initRecyclerView();
     }
 
-    private void initRecyclerView(){
+    private void initRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.oldThingsRecyclerView);
         RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(contacts, this);
         recyclerView.setAdapter(recyclerViewAdapter);
@@ -163,20 +164,18 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        if(view == sortButton){
-            //todo: sort things by either newest or oldest
+        if (view == sortButton) {
+            //sorts Things from oldest to newest
             contacts = new ArrayList<>();
             dbref = FirebaseDatabase.getInstance().getReference(user);
             dbref.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     List<DataSnapshot> L = new ArrayList<>();
-                    for(DataSnapshot d: dataSnapshot.getChildren()){
+                    for (DataSnapshot d : dataSnapshot.getChildren()) {
                         L.add(d);
                     }
-                    // Implementation of sortList()
-                    List<GoodThings> publishList = new ArrayList<>();
-                    // My top posts by number of stars
+                    // Orders notes by Date
                     Query notesOrderedByDate = dbref.child(user).orderByChild("date");
                     notesOrderedByDate.addChildEventListener(new ChildEventListener() {
                         @Override
@@ -207,28 +206,29 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                         // ...
                     });
                     List<DataSnapshot> List = new ArrayList<>();
-                    for(DataSnapshot d: dataSnapshot.getChildren()){
+                    for (DataSnapshot d : dataSnapshot.getChildren()) {
                         List.add(d);
                     }
+                    //creates contacts from notes
                     for (int i = 0; i < List.size(); i++) {
-                        contacts.add(new Contact (List.get(i).getValue(GoodThings.class).getThingOne()));
-                        contacts.add(new Contact (List.get(i).getValue(GoodThings.class).getThingTwo()));
-                        contacts.add(new Contact (List.get(i).getValue(GoodThings.class).getThingThree()));
+                        contacts.add(new Contact(List.get(i).getValue(GoodThings.class).getThingOne()));
+                        contacts.add(new Contact(List.get(i).getValue(GoodThings.class).getThingTwo()));
+                        contacts.add(new Contact(List.get(i).getValue(GoodThings.class).getThingThree()));
                     }
+                    //adds contacts to recycler view
                     initRecyclerView();
                 }
+
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                 }
             });
             initRecyclerView();
 
-        }
-        else if(view == notificationsButton){
+        } else if (view == notificationsButton) {
             //todo: configure notifications that recycle saved things back to user
 
-        }
-        else if(view == toHomeButton){
+        } else if (view == toHomeButton) {
             Intent HomePageIntent = new Intent(activity_old_notes.this, ThreeGoodThings.class);
             startActivity(HomePageIntent);
         }
@@ -242,6 +242,9 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
                 .setSmallIcon(android.R.drawable.stat_notify_more)
                 .setAutoCancel(true);
     }
+}
+
+
     /*
     // IDEA: Sort list of Good Things by data, newest to oldest, to be published to screen in
     // initContacts() function. Uses for loops to check if Good Things in initContacts() are ordered
@@ -376,43 +379,7 @@ public class activity_old_notes extends AppCompatActivity implements View.OnClic
         return (ArrayList<GoodThings>) gtList;
 
     }
-*/
 
-    /*
-                Random rand = new Random();
-                int randIndex = rand.nextInt(L.size());
-                GoodThings goodThingsChosen = new GoodThings();
-                goodThingsChosen = L.get(randIndex).getValue(GoodThings.class);
-                contacts.add(new Contact(goodThingsChosen.getThingOne()));
-                contacts.add(new Contact(goodThingsChosen.getThingTwo()));
-                contacts.add(new Contact(goodThingsChosen.getThingThree()));
-                thingOne = goodThingsChosen.getThingOne();
-                Random rand2 = new Random();
-                int randIndex2 = rand2.nextInt(L.size());
-                GoodThings goodThingsChosen2 = new GoodThings();
-                goodThingsChosen2 = L.get(randIndex2).getValue(GoodThings.class);
-                //if below ensures that the same set of things isn't selected twice
-                if(goodThingsChosen2.getThingOne() == thingOne){
-                    rand2 = new Random();
-                }
-                randIndex2 = rand2.nextInt(L.size());
-                goodThingsChosen2 = L.get(randIndex2).getValue(GoodThings.class);
-                contacts.add(new Contact(goodThingsChosen2.getThingOne()));
-                contacts.add(new Contact(goodThingsChosen2.getThingTwo()));
-                contacts.add(new Contact(goodThingsChosen2.getThingThree()));
-                thingTwo = goodThingsChosen2.getThingOne();
-                Random rand3 = new Random();
-                int randIndex3 = rand3.nextInt(L.size());
-                GoodThings goodThingsChosen3 = new GoodThings();
-                goodThingsChosen3 = L.get(randIndex3).getValue(GoodThings.class);
-                //if below ensures that the same set of things isn't selected twice
-                if(goodThingsChosen3.getThingOne() == thingOne || goodThingsChosen3.getThingOne() == thingTwo){
-                    rand3 = new Random();
-                }
-                randIndex3 = rand3.nextInt(L.size());
-                goodThingsChosen3 = L.get(randIndex3).getValue(GoodThings.class);
-                contacts.add(new Contact(goodThingsChosen3.getThingOne()));
-                contacts.add(new Contact(goodThingsChosen3.getThingTwo()));
-                contacts.add(new Contact(goodThingsChosen3.getThingThree()));
-                */
+
 }
+*/
