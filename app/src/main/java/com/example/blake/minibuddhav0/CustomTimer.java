adf2f1626a0406aa960a04dfe495bcedf80b09c2
ljschaef <ljschaef@umich.edu>
2018-12-10 09:32:48 -0500

    CustomTimer may be done. Need to check. My computer won't build

--- app/src/main/java/com/example/blake/minibuddhav0/CustomTimer.java
+++ app/src/main/java/com/example/blake/minibuddhav0/CustomTimer.java
@@ -1,6 +1,7 @@
 package com.example.blake.minibuddhav0;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +11,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class CustomTimer extends AppCompatActivity {
+public class CustomTimer extends AppCompatActivity implements View.OnClickListener {
 
     private Button startButton;
     private EditText customTimeText;
     private TextView customTimeTextView;
+    private CountDownTimer countDownTimer;
+    private int customTime;
+    private long startTimeInMillis;
+    private long timeLeftInMilliseconds = startTimeInMillis;
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,65 @@ public class CustomTimer extends AppCompatActivity {
         setContentView(R.layout.activity_custom_timer);
 
         startButton = findViewById(R.id.customStartButton);
+        customTimeText = findViewById(R.id.customTimeText);
+        customTimeTextView = findViewById(R.id.customTimeTextView);
+
+        customTime = Integer.parseInt(customTimeText.getText().toString());
+        startTimeInMillis = customTime * 6000;
+
+        startButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        if(timerRunning){
+            stopTimer();
+        }
+        else{
+            startTimer();
+        }
+    }
+
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMilliseconds = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+                startButton.setText("START");
+
+            }
+        }.start();
+        startButton.setText("PAUSE");
+        timerRunning = true;
+    }
+
+    public void stopTimer(){
+        countDownTimer.cancel();
+        startButton.setText("START");
+        timerRunning = false;
+    }
+
+    public void updateTimer(){
+        int minutes = (int) timeLeftInMilliseconds / 60000;
+        int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
+
+        String timeLeftText = timeParser(minutes, seconds);
+        customTimeTextView.setText(timeLeftText);
+    }
+
+    public String timeParser(int minutes, int seconds){
+        String timeLeftText;
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if(seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+        return timeLeftText;
+    }
 }
